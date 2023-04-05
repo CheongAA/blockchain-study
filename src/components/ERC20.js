6af0f00c3b04387a8d26e2038e891fe15c2eb3bf
Cheong A.Kim <cheonga@neowiz.com>
2023-04-05 16:59:27 +0900

    feat: ecr20 transfer 추가

--- src/components/ERC20.js
+++ src/components/ERC20.js
@@ -11,6 +11,8 @@ const ERC20 = () => {
   );
 
   const balanceOfAddressInputRef = useRef();
+  const addressInputRef = useRef();
+  const amountInputRef = useRef();
 
   const [data, setData] = useState({
     name: "",
@@ -24,6 +26,45 @@ const ERC20 = () => {
       ? await contract.methods[method](param).call()
       : await contract.methods[method]().call();
     setData((prev) => ({ ...prev, [method]: res }));
+  };
+
+  const getGasprice = async () => {
+    const { fast, slow } = await (
+      await fetch("https://ethgasstation.info/api/ethgasAPI.json")
+    ).json();
+    return { fast, slow };
+  };
+
+  const transferToken = async () => {
+    const data = await contract.methods
+      .transfer(addressInputRef.current?.value, amountInputRef.current?.value)
+      .encodeABI();
+    const estimateGas = await contract.methods
+      .transfer(addressInputRef.current?.value, amountInputRef.current?.value)
+      .estimateGas({ from: process.env.REACT_APP_ADDRESS });
+    const nonce = await web3.eth.getTransactionCount(
+      process.env.REACT_APP_ADDRESS
+    );
+    const { fast } = await getGasprice();
+
+    const txObj = {
+      nonce,
+      data,
+      to: process.env.REACT_APP_TOKEN_CONTRACT_ADDRESS,
+      gasLimit: estimateGas,
+      gasPrice: web3.utils.toHex(web3.utils.toWei(fast.toString(), "gwei")),
+    };
+
+    const signed = await web3.eth.accounts.signTransaction(
+      txObj,
+      process.env.REACT_APP_ADDRESS_PK
+    );
+    if (signed?.rawTransaction) {
+      const result = await web3.eth.sendSignedTransaction(
+        signed.rawTransaction
+      );
+      alert(result.transactionHash);
+    }
   };
 
   return (
@@ -50,6 +91,16 @@ const ERC20 = () => {
           }
         >
           getBalanceOf
+        </button>
+      </div>
+
+      <hr />
+      <div>
+        <label htmlFor="address">address</label>
+        <input id="address" ref={addressInputRef} type="text" />
+        <input ref={amountInputRef} type="number" />
+        <button type="button" onClick={transferToken}>
+          transfer
         </button>
       </div>
     </div>
